22c334e7e33fc29fccaa82109fb58677097f1445
aashrithbandaru <43557311+aashrithbandaru@users.noreply.github.com>
2021-06-26 23:47:55 -0700

    lottastuff

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID= "b4f5b86b-d7d7-4c56-9185-29a9ba01d6a2"
+            userName="aashrithbandaru"
+            userSecret="flameo hotman"
+        />
+    );
+}
+
+export default App;
